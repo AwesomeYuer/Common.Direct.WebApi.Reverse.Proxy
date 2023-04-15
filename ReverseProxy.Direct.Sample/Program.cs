c7fe4024df2063c794bf84b728e3890f168d06c5
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-04-15 18:56:19 +0800

    1

--- ReverseProxy.Direct.Sample/Program.cs
+++ ReverseProxy.Direct.Sample/Program.cs
@@ -1,29 +1,10 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
 
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace Yarp.Sample
-{
-    /// <summary>
-    /// Class that contains the entrypoint for the Reverse Proxy sample app.
-    /// </summary>
-    public class Program
-    {
-        /// <summary>
-        /// Entrypoint of the application.
-        /// </summary>
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
 
-        private static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+
+var app = builder.Build();
+
+
+
+app.Run();
