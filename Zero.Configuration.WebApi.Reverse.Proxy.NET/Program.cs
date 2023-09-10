f219359ef8ec44564ba1bedef4e148656fd9e10b
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-09-11 07:37:22 +0800

    12121212

--- Zero.Configuration.WebApi.Reverse.Proxy.NET/Program.cs
+++ Zero.Configuration.WebApi.Reverse.Proxy.NET/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddHttpForwarder();
 
 var app = builder.Build();
 
+app.UseFileServer();
+
 string proxyPathBaseString = builder.Configuration.GetValue(nameof(proxyPathBaseString), "api/proxy");
 
 app.UseRouting();
