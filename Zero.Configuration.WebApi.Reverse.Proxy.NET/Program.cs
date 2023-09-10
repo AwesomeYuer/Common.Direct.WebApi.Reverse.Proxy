e00d365f0314665456e8f7f5d6d86ade1c5d0c03
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-09-11 05:03:33 +0800

    1

--- Zero.Configuration.WebApi.Reverse.Proxy.NET/Program.cs
+++ Zero.Configuration.WebApi.Reverse.Proxy.NET/Program.cs
@@ -1,107 +1,14 @@
-using System.Diagnostics;
-using Yarp.ReverseProxy.Forwarder;
-using Yarp.ReverseProxy.Transforms;
+using Microshaoft;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var secretPathSegment = builder.Configuration["secretPathSegment"];
-
 builder.Services.AddHttpForwarder();
 
-// Configure our own HttpMessageInvoker for outbound calls for proxy operations
-var httpMessageInvoker = new HttpMessageInvoker
-                            (
-                                new SocketsHttpHandler()
-                                {
-                                      UseProxy                  = false
-                                    , AllowAutoRedirect         = false
-                                    // , AutomaticDecompression    = DecompressionMethods.All
-                                    // , UseCookies                = false
-                                    , ConnectTimeout            = TimeSpan.FromSeconds(15)
-                                    , ActivityHeadersPropagator = new ReverseProxyPropagator
-                                                                        (
-                                                                            DistributedContextPropagator
-                                                                                                    .Current
-                                                                        )
-                                }
-                            );
-
-var requestOptions = new ForwarderRequestConfig
-{
-    ActivityTimeout = TimeSpan.FromSeconds(300)
-};
 var app = builder.Build();
 
+var secretPathSegment = builder.Configuration["secretPathSegment"];
+app.UseZeroConfigurationWebApiReverseProxy(pathBaseString: secretPathSegment);
+
 app.UseRouting();
-
-var httpForwarder = app.Services.GetService<IHttpForwarder>();
-
-// When using IHttpForwarder for direct forwarding you are responsible for routing, destination discovery, load balancing, affinity, etc..
-// For an alternate example that includes those features see BasicYarpSample.
-app
-    .UseEndpoints
-        (
-            (endpoints) =>
-            {
-                endpoints
-                        .Map
-                            (
-                                $"{secretPathSegment}/{{**catch-all}}"
-                                , async (httpContext) =>
-                                {
-                                    var request = httpContext.Request;
-                                    var pathSegments = request.Path.Value.Split('/');
-
-                                    var destinationScheme = pathSegments[2];
-                                    var destinationBaseAddress = pathSegments[3];
-
-                                    var pathPrefix = $"/{secretPathSegment}/{destinationScheme}/{destinationBaseAddress}";
-
-                                    var destinationPrefix = $"{destinationScheme}://{destinationBaseAddress}";
-                                    var destinationPath = request.Path.Value![pathPrefix.Length..];
-
-                                    var forwarderError =
-                                            await httpForwarder
-                                                    .SendAsync
-                                                        (
-                                                              httpContext
-                                                            , destinationPrefix
-                                                            , httpMessageInvoker
-                                                            , requestOptions
-                                                            , (context, forwardRequest) =>
-                                                            {
-                                                                // Customize the query string:
-                                                                var queryTransformContext =
-                                                                        new QueryTransformContext(context.Request);
-                                                                // Assign the custom uri. Be careful about extra slashes when concatenating here. RequestUtilities.MakeDestinationAddress is a safe default.
-                                                                forwardRequest
-                                                                    .RequestUri =
-                                                                        RequestUtilities
-                                                                            .MakeDestinationAddress
-                                                                                    (
-                                                                                          destinationPrefix
-                                                                                        , new PathString(destinationPath)
-                                                                                        , queryTransformContext.QueryString
-                                                                                    );
-                                                                // Suppress the original request header, use the one from the destination Uri.
-                                                                forwardRequest.Headers.Host = null;
-                                                                return default;
-                                                            }
-                                                        );
-
-                                    // Check if the proxy operation was successful
-                                    if 
-                                        (forwarderError != ForwarderError.None)
-                                    {
-                                        var forwarderErrorFeature =
-                                                    httpContext
-                                                            .Features
-                                                            .Get<IForwarderErrorFeature>();
-                                        var exception = forwarderErrorFeature.Exception;
-                                    }
-                                }
-                            );
-            }
-        );
 
 app.Run();
---
+++ Zero.Configuration.WebApi.Reverse.Proxy.NET/ZeroConfigurationWebApiReverseProxyAppExtensions.cs
@@ -0,0 +1,136 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Yarp.ReverseProxy.Forwarder;
+using Yarp.ReverseProxy.Transforms;
+
+namespace Microshaoft;
+
+public static class ZeroConfigurationWebApiReverseProxyAppExtensions
+{
+    public static void UseZeroConfigurationWebApiReverseProxy
+                            (
+                                this WebApplication @this
+                                , Func<HttpRequest, ForwarderError> onAuthenticationProcessFunc = null!
+                                , string pathBaseString = "api/proxy"
+                            )
+    {
+        pathBaseString = pathBaseString.Trim().Trim('/');
+
+#pragma warning disable CA2000 // Dispose objects before losing scope
+        var socketsHttpHandler = new SocketsHttpHandler()
+        {
+            UseProxy = false
+           , AllowAutoRedirect = false
+           // , AutomaticDecompression    = DecompressionMethods.All
+           // , UseCookies                = false
+           , ConnectTimeout = TimeSpan.FromSeconds(15)
+           , ActivityHeadersPropagator = new ReverseProxyPropagator
+                                               (
+                                                   DistributedContextPropagator
+                                                                           .Current
+                                               )
+        };
+
+        var httpMessageInvoker = new HttpMessageInvoker(socketsHttpHandler);
+#pragma warning restore CA2000 // Dispose objects before losing scope
+
+        var requestOptions = new ForwarderRequestConfig
+        {
+            ActivityTimeout = TimeSpan.FromSeconds(300)
+        };
+
+        var httpForwarder = @this.Services.GetService<IHttpForwarder>();
+
+        var a = pathBaseString.Split('/');
+        var p = a.Length;
+
+        // When using IHttpForwarder for direct forwarding you are responsible for routing, destination discovery, load balancing, affinity, etc..
+        // For an alternate example that includes those features see BasicYarpSample.
+        @this
+            .UseEndpoints
+                (
+                    (endpoints) =>
+                    {
+                        endpoints
+                                .Map
+                                    (
+                                        $"{pathBaseString}/{{**catch-all}}"
+                                        , requestDelegate: async (httpContext) =>
+                                        {
+                                            if (onAuthenticationProcessFunc != null)
+                                            {
+                                                if
+                                                    (
+                                                        ForwarderError.None
+                                                        !=
+                                                        onAuthenticationProcessFunc(httpContext.Request)
+                                                    )
+                                                {
+                                                    httpContext.Response.StatusCode = 401;
+                                                    return;
+                                                }
+                                            }
+
+                                            var request = httpContext.Request;
+                                            var requestPathSegments = request.Path.Value!.Split('/');
+
+                                            var destinationScheme = requestPathSegments[p + 1];
+                                            var destinationBaseAddress = requestPathSegments[p + 2];
+
+                                            var pathPrefix = $"/{pathBaseString}/{destinationScheme}/{destinationBaseAddress}";
+
+                                            var destinationPrefix = $"{destinationScheme}://{destinationBaseAddress}";
+                                            var destinationPath = request.Path.Value![pathPrefix.Length..];
+
+                                            var forwarderError =
+                                                    await httpForwarder!
+                                                            .SendAsync
+                                                                (
+                                                                      httpContext
+                                                                    , destinationPrefix
+                                                                    , httpMessageInvoker
+                                                                    , requestOptions
+                                                                    , (context, forwardRequest) =>
+                                                                    {
+                                                                        // Customize the query string:
+                                                                        var queryTransformContext =
+                                                                                new QueryTransformContext(context.Request);
+                                                                        // Assign the custom uri. Be careful about extra slashes when concatenating here. RequestUtilities.MakeDestinationAddress is a safe default.
+                                                                        forwardRequest
+                                                                            .RequestUri =
+                                                                                RequestUtilities
+                                                                                    .MakeDestinationAddress
+                                                                                            (
+                                                                                                  destinationPrefix
+                                                                                                , new PathString(destinationPath)
+                                                                                                , queryTransformContext.QueryString
+                                                                                            );
+                                                                        // Suppress the original request header, use the one from the destination Uri.
+                                                                        forwardRequest.Headers.Host = null;
+                                                                        return default;
+                                                                    }
+                                                                );
+
+                                            // Check if the proxy operation was successful
+                                            if
+                                                (forwarderError != ForwarderError.None)
+                                            {
+                                                var forwarderErrorFeature =
+                                                            httpContext
+                                                                    .Features
+                                                                    .Get<IForwarderErrorFeature>();
+                                                var exception = forwarderErrorFeature!.Exception;
+                                            }
+                                        }
+                                    );
+                    }
+                )
+            ;
+    }
+}
